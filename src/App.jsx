60353377cbb8e825d9d26e8177ffae6fe4015f88
Remy349 <santoraga15@gmail.com>
2022-11-10 23:34:04 -0600

    Structure completed, just need to add styles and form validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,69 @@
 import React from 'react'
+import { IntroContent } from './components/IntroContent'
 
-function App() {
-  return (
-    <main className='main'>
-      <h1>OK!!!</h1>
-    </main>
-  )
+class App extends React.Component {
+  handleSubmit(e) {
+    e.preventDefault()
+  }
+
+  render() {
+    return (
+      <main className='intro'>
+        <IntroContent />
+        <div className='intro__forms'>
+          <div>
+            <div className='intro__forms-content'>
+              <p className='intro__forms-content_text'>
+                Try it free 7 days <span>then $20/mo. thereafter</span>
+              </p>
+            </div>
+          </div>
+          <div>
+            <form onClick={this.handleSubmit} className='intro__form'>
+              <div className='intro__form-container'>
+                <input
+                  type='text'
+                  className='intro__form-input'
+                  autoComplete='off'
+                  placeholder='First Name'
+                />
+              </div>
+              <div className='intro__form-container'>
+                <input
+                  type='text'
+                  className='intro__form-input'
+                  autoComplete='off'
+                  placeholder='Last Name'
+                />
+              </div>
+              <div className='intro__form-container'>
+                <input
+                  type='text'
+                  className='intro__form-input'
+                  autoComplete='off'
+                  placeholder='Email Address'
+                />
+              </div>
+              <div className='intro__form-container'>
+                <input
+                  type='password'
+                  className='intro__form-input'
+                  placeholder='Password'
+                />
+              </div>
+              <button className='intro__form-submit' type='submit'>
+                Claim your free trial
+              </button>
+              <p className='intro__form-terms'>
+                By clicking the button, you are agreeing to our
+                <span>Terms and Services</span>
+              </p>
+            </form>
+          </div>
+        </div>
+      </main>
+    )
+  }
 }
 
 export default App
---
+++ src/components/IntroContent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const IntroContent = () => {
+  return (
+    <div className='intro__content'>
+      <h1 className='intro__content-title'>Learn to code by watching others</h1>
+      <p className='intro__content-desc'>
+        See how experienced developers solve problems in real-time. Watching
+        scripted tutorials is great, but understanding how developers think is
+        invaluable.
+      </p>
+    </div>
+  )
+}
